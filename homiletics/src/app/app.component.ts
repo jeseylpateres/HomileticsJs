b26f5a42cf3b771a8f370d0d24874e1f179b0e72
jeseylpateres <jeseylpateres@users.noreply.github.com>
2017-10-21 18:18:53 +0800

    update

--- homiletics/src/app/app.component.ts
+++ homiletics/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-//import { LoginPage } from '../pages/login/login';
-//import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
