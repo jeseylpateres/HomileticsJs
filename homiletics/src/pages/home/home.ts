2412724c0988df52154d5c0a062b81d55145ac14
jeseylpateres <jeseylpateres@users.noreply.github.com>
2017-10-21 19:13:02 +0800

    added

--- homiletics/src/pages/home/home.ts
+++ homiletics/src/pages/home/home.ts
@@ -24,7 +24,7 @@ export class HomePage {
 
 
   constructor(public navCtrl: NavController, public app: App) {
-      this.book = new Book();
+      //this.book = new Book();
   }
 
   menu(){
---
+++ homiletics/src/pages/model/book.ts
@@ -0,0 +1,72 @@
+export class Book {
+    genesis: string;
+    exodus: string;
+    leviticus: string;
+    numbers: string;
+    deuteronomy: string;
+    joshua: string;
+    judges: string;
+    ruth: string;
+    samuel: string;
+    firstkings: string;
+    secondkings: string;
+    firstchronicles: string;
+    secondchronicles: string;
+    ezra: string;
+    nehemiah: string;
+    esther: string;
+    job: string;
+    psalms: string;
+    proverbs: string;
+    ecclesiastes: string;
+    songofsolomon: string;
+    isaiah: string;
+    jeremiah: string;
+    lamentations: string;
+    ezekiel: string;
+    daniel: string;
+    hosea: string;
+    joel: string;
+    amos: string;
+    obadiah: string;
+    jonah: string;
+    micah: string;
+    nahum: string;
+    kabakkuk: string;
+    zephaniah: string;
+    haggai: string;
+    zechariah: string;
+    malachi: string;
+    
+
+    matthew: string;
+    mark: string;
+    luke: string;
+    john: string;
+    acts: string;
+    romans: string;
+    firstcorinthians: string;
+    secondcorinthians: string;
+    galatians: string;
+    ephesians: string;
+    philippians: string;
+    colossians: string;
+    firstthessalonians: string;
+    secondthessalonians: string;
+    firsttimothy: string;
+    secondtimonthy: string;
+    titus: string;
+    philemon: string;
+    hebrews: string;
+    james: string;
+    firstpeter: string;
+    secondpeter: string;
+    firstjohn: string;
+    secondjohn: string;
+    third: string;
+    jude: string;
+    revelation: string;
+    
+
+    
+}
